d1a2677df30794fa619c86d9ea47c4786963a156
Leshoraa <leshoraa@gmail.com>
2026-06-21 17:10:06 +0700

    Review

--- Sandi/TampilkanData.h
+++ Sandi/TampilkanData.h
@@ -3,6 +3,12 @@
 
 #include <string>
 
+
+/* NOTE
+* ! JANGAN bikin struct di .h dan deklarasinya.
+*
+*/
+
 // Struct untuk data Bengkel (Maksimal 100 data di Array)
 struct Bengkel {
     int id;
@@ -10,9 +16,16 @@ struct Bengkel {
     std::string daerah;
 };
 
+/* NOTE
+* ? Kamu yakin pake extern? Udah dibahas kah sama dosennya?
+*
+* Better sesuaiin sama materi yang dikasih aja,
+* Biar nanti gampang pas dipresentasiin.
+*/
+
 // Deklarasi Global Array untuk data Bengkel agar bisa diakses bareng-bareng
 extern Bengkel databaseBengkel[100];
-extern int jumlahBengkel;
+extern int jumlahBengkel; 
 
 // Prototipe Fungsi Utama Sandi (Menu 3)
 void tampilkanSemuaData();
